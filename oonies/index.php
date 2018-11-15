4bbfe702dc4ee0ccc28e5b3c2dd807cedcccace3
ChayOk <45060686+ChayOk@users.noreply.github.com>
2018-11-15 21:55:05 +0300

    Add files via upload

---
+++ oonies/index.php
@@ -0,0 +1,343 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="css/slider.css">
+	<link rel="stylesheet" type="text/css" href="css/slider-dots.css">
+	<link href="images/favicon.png" rel="shortcut icon" type="image/x-icon"><!--Иконка странички-->
+	<link rel="stylesheet" type="text/css" href="css/animate.css"> <!--подключение библиотеки Animate-->
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+	<title>Oonies (Onoies) - Детский конструктор из шариков. Официальный сайт Oonies (Onoies). Купить детский конструктор в Минске. Доставка по всей Беларуси.</title>
+</head>
+
+<body>
+<header class="head-0">
+	<div class="main-top">                      <!--Верхний главный блок-->
+		<div class="header-t clearfix">                  <!--Верхний блок-->
+			<p class="asus-logo"><a href="#top"><img src="images/oonieslogo.png" alt="Oonies" id="logo"></a></p>
+			<nav>
+				<ul class="menu">
+					<li class="active">Oonies</li>
+					<li><a href="#char">Комплектация</a></li>
+					<li><a href="#preim">Преимущества</a></li>
+					<li><a href="#video">Видео</a></li>
+					<li><a href="#zak">Заказать</a></li>
+					<li><a href="tel:+375298571237">+375(29)857-12-37</a></li>
+				</ul>
+			</nav>
+		</div> 
+	</div>
+</header>
+    <!-----       СЛАЙДЕР        ----->
+<div class="my-slider">
+	<ul id="top">
+		<li>
+			<div>
+				<div class="slide-cover-1">
+                    <p><span>Получите</span><strong> Oonies </strong> <span>всего за <s>89р.</s></span> <strong>43p.</strong> <br>
+                </p>
+					<a href="#zak" class="download-top">ВЗЯТЬ НАБОР ПО АКЦИИ</a>
+					
+				</div>
+				 <img 
+      class="data-img"src="images/slide1_350.png"
+      data-sml="images/slide1_350.png"
+      data-med="images/slide1_700.png"
+      data-lrg="images/slide1.jpg"
+    >
+			</div>
+		</li>
+		<li>
+			<div>
+				<div class="slide-cover-2">
+				<p><span>Получите</span><strong> Oonies</strong> <span>всего за <s>89р.</s></span> <strong>43p.</strong> <br>
+                </p>
+					<a href="#zak" class="download-top">ВЗЯТЬ НАБОР ПО АКЦИИ</a>
+				</div>
+				 <img 
+      class="data-img"src="images/slide2_350.png"
+      data-sml="images/slide2_350.png"
+      data-med="images/slide2_700.png"
+      data-lrg="images/slide2.jpg"
+    >
+			</div>
+		</li>
+		<li>
+			<div>
+				<div class="slide-cover-3">
+				<p><span>Получите</span><strong> Oonies</strong> <span>всего за <s>89р.</s></span> <strong>43p.</strong> <br>
+                </p>
+					<a href="#zak" class="download-top">ВЗЯТЬ НАБОР ПО АКЦИИ</a>
+				</div>
+				 <img 
+      class="data-img"src="images/slide3_350.png"
+      data-sml="images/slide3_350.png"
+      data-med="images/slide3_700.png"
+      data-lrg="images/slide3.jpg"
+    >
+			</div>
+		</li>
+	</ul>
+</div>	
+	<!-----       /СЛАЙДЕР        ----->
+<div class="main-middle-3-all wow fadeInRight">	
+	<h3 id="char">Комплектация конструктора</h3>
+	<hr>
+	<div class="main-middle-3 main-top clearfix">           <!--товар-->
+		<article>
+            <img src="images/blok_kompl.jpg" alt="Oonies-детский конструктор" class="blok_kompl">
+			<ul>
+                <li>Насос для производства шариков</li>
+                <li>36 заготовок для таких шариков</li>
+                <li>18 украшений-аксессуаров</li>
+                <li>12 глаз для будущих игрушек</li>
+                <li>Инструция</li>
+            </ul>
+            
+		</article>
+	</div>
+</div>	
+<!-----       ТОВАРЫ        ----->
+<section class="main-middle-4-section wow fadeInLeft" id="preim">
+    <h3>Конструктор из шариков Oonies (Onoies) и его ПРЕИМУЩЕСТВА:</h3>
+	<hr>
+    <div class="middle-4-article clearfix">
+        <article>
+            <img src="images/oonies001.png" alt="" class="swaying">
+            <div class="middle-4-material">
+                <div class="level-0">
+					<p>Развивает воображение и фантазию у ребенка,
+                        а также улучшает мелкую маторику. У ребенка появляется слуховая 
+                        и зрительная память. Он будет сконцентрирован на занятии.
+					</p>
+                </div>
+            </div>
+        </article>
+        <article>
+            <img src="images/oonies002.png" alt="" class="swaying">
+            <div class="middle-4-material">
+                <div class="level-0">
+					<p>Возможость изготовления фигур, животных и конструкций.
+                        Игрушка подходит для детей в возрасте от 3-х лет.
+                	</p>
+                </div>
+            </div>
+        </article>
+        <article>
+            <img src="images/oonies003.png" alt="" class="swaying">
+            <div class="middle-4-material">
+                <div class="level-0">
+					<p>Шарики создаются из безопасного для детей материала. После надувания,
+                        они склеиваются между собой без премения клея и средств.
+                	</p>
+                </div>
+            </div>
+        </article>
+        <article>
+            <img src="images/oonies004.png" alt="" class="swaying">
+            <div class="middle-4-material">
+                <div class="level-0">
+					<p>Наличие аксессуаров и украшений позволит создавать композиции любого типа.
+                        Шарики не лопаются и с ними можно постоянно играть.
+					</p>
+                </div>
+            </div>
+        </article>
+    </div>
+</section>
+    
+<!-----       /ТОВАРЫ        ----->
+    
+<!-----       ДОСТАВКА        ----->
+    <div class="main-middle-5-all wow fadeInRight">	
+	<h3 id="char">Доставка и оплата</h3>
+	<hr>
+	<div class="main-middle-5 main-top clearfix">           <!--товар-->
+		<article>
+			<img src="images/dostavka-1.png" alt="">
+			<p><span>Бесплатный самовывоз</span><br>
+                Чижовка-Арена<br>
+                По предварительному согласованию
+			</p>
+		</article>
+		<article>
+			<img src="images/dostavka-2.png" alt="">
+            <p>
+                <span>Курьерская доставка<br>
+                    по Минску.</span><br>
+                В остальные города Беларуси<br>
+                доставка почтой.<br>
+            </p>
+		</article>
+		<article>
+			<img src="images/dostavka-3.png" alt="" class="0wow fadeInLeft" data-wow-delay="1.5s">
+			 <p>
+                 <span>Оплата</span> производится наличными<br>
+                 либо банковской картой.<br>
+             </p>
+		</article>
+	</div>
+</div>
+<!-----       /ДОСТАВКА        ----->
+<!-----       VIDEO        ----->
+	<section class="video-asus">
+		<div class="video-center main-top"> <!-- map-ifr -->
+			<h2 id="video">Посмотрите, как работает Oonies (Onoies)</h2>
+            <div class="video">
+			<iframe width="560" height="370" src="https://www.youtube.com/embed/EeIIdvHIP-E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="video1"></iframe>
+            <iframe width="560" height="370" src="https://www.youtube.com/embed/_ukUcQd_Yws" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="video2"></iframe>
+            </div>
+		</div>
+	</section>
+<!-----       /VIDEO        ----->
+<!-----       Нравится ли Вам ноутбук ?        ----->
+<div class="drivers-100">
+	<div class="drivers-1166 main-top clearfix">
+		<h3>Хотите заказать детский коструктор Oonies (Onoies) ?</h3>
+		<p><a href="tel:+375298571237" class="order-now">ПОЗВОНИТЕ НАМ</a></p>
+	</div>
+</div>	
+	
+<!-----       /Нравится ли Вам ноутбук ?        ----->
+<!-----       Контакты        ----->	
+<section class="contact-asus">
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2353.914017186377!2d27.62699761582614!3d53.844393180086364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dbd2192470e203%3A0x391415e3b00eeeb4!2z0KfQuNC20L7QstC60LAt0JDRgNC10L3QsA!5e0!3m2!1sru!2sby!4v1541523970449" width="100%" height="570" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <div class="email">
+
+                <?php
+            $urok="Oonies - Детский конструктор";
+            error_reporting( E_ERROR );   //Отключение предупреждений и нотайсов (warning и notice) на сайте
+            // создание переменных из полей формы		
+            if (isset($_POST['name1']))			{$name1			= $_POST['name1'];		if ($name1 == '')	{unset($name1);}}
+           
+            if (isset($_POST['tel']))			{$tel			= $_POST['tel'];		if ($tel == '')	    {unset($tel);}}
+            if (isset($_POST['text']))			{$text			= $_POST['text'];		if ($text == '')	{unset($text);}}
+            if (isset($_POST['sab']))			{$sab			= $_POST['sab'];		if ($sab == '')		{unset($sab);}}
+            //стирание треугольных скобок из полей формы
+            if (isset($name1) ) {
+            $name1=stripslashes($name1);
+            $name1=htmlspecialchars($name1);
+            }
+            if (isset($tel) ) {
+            $email1=stripslashes($tel);
+            $email1=htmlspecialchars($tel);
+            }
+            if (isset($text) ) {
+            $text=stripslashes($text);
+            $text=htmlspecialchars($text);
+            }
+            // адрес почты куда придет письмо
+            $address="tovar-ka18@yandex.by";
+            // текст письма 
+            $note_text="Тема : $urok \r\nИмя : $name1 \r\n Phone number : $tel \r\n Дополнительная информация : $text";
+
+            if (isset($name1) && isset ($tel) &&  isset ($sab) ) {
+            mail($address,$urok,$note_text,"Content-type:text/plain; windows-1251"); 
+            // сообщение после отправки формы
+            echo "<p>Спасибо за заказ. <br> Вам скоро перезвонят.</p>";
+            }
+
+            ?>
+        </div>
+	<article class="contact-form  0wow flipInX" data-wow-offset="200" data-wow-iteration="1">
+		<h2>Заказать</h2><a name="zak"></a>
+		<div class="foot-text">
+			<form action="index.php#zak" method="post" name="f1"> 
+    			<input type="text" class="foot-email" placeholder="Ваше имя *" required name="name1">
+                <input type="tel" class="foot-email" placeholder="Ваш телефон *" required name="tel">
+				<textarea class="foot-textarea" placeholder="Ваш текст" name="text"></textarea>
+				<input type="submit" class="foot-submit" value="Заказать" name="sab">
+			</form>
+		</div>
+	</article>
+</section>
+<!-----       /Контакты        ----->	
+<!-----       Footer        ----->
+<footer class="foot">
+	<div class="middle-4-article main-top clearfix">
+		 <h6><a href="https://vk.com/i__chayok" target="_blank">Copyright © 2018</a> </h6>
+	</div>
+</footer>	
+	
+<!-----       /Footer        ----->	
+    
+<script src="js/jquery-2.2.2.min.js"></script> <!--подключение библиотеки jquery-->
+<script src="js/slider.js"></script>           <!--код js для слайдера-->
+	<script>
+		jQuery(document).ready(function($) { /*настройки слайдера*/
+			$('.my-slider').unslider({
+			nav:true,                        /*навигация(точки)| значия thue/false*/
+			autoplay:true,                   /*автопроигрывание| значия thue/false*/
+			arrows:false,                    /*управление стрелочками| значия thue/false*/
+			delay:4000,                      /*скорость смены кадров|3000 по-умолчанию*/
+			speed:750,                       /*скорость перехода между кадрами|чем меньше цифра, тем больше скорость */
+			animation:'horizontal',/*'horizontal' 'vertical' */
+			});
+		});
+	</script>
+<!-----      Выравнивание колонок         ----->	
+	<script>
+	function setEqualHeight(columns) { 
+		var heightcolumn = 0; 
+		columns.each( function() { 
+			currentHeight = $(this).height(); 
+			if(currentHeight > heightcolumn) { heightcolumn = currentHeight; } } ); 
+			columns.height(heightcolumn); 
+		} 
+		
+		$(document).ready(function() { setEqualHeight($(".level-0")); });
+</script>
+<!-----      /Выравнивание колонок        .level-0 -----> 
+	<!---- resolution-img.js ----->
+	  <script src="js/resolution-img.js"></script> 
+
+  <script>
+  $(function(){
+    $('.data-img').dataImg({
+      sml: 350,
+      med: 700,
+      lrg: 1050,
+      resize: true,
+      bgImg: true
+    });
+  });
+  </script>
+	<!---- /resolution-img.js ----->
+	<!---- wow.min.js ----->
+	<script src="js/wow.min.js"></script>
+	<script>
+		new WOW().init();
+	</script>
+	<!---- /wow.min.js ----->
+
+    <script>
+        (function() {
+
+  "use strict";
+
+  var toggles = document.querySelectorAll(".cmn-toggle-switch");
+
+  for (var i = toggles.length - 1; i >= 0; i--) {
+    var toggle = toggles[i];
+    toggleHandler(toggle);
+  };
+
+  function toggleHandler(toggle) {
+    toggle.addEventListener( "click", function(e) {
+      e.preventDefault();
+      (this.classList.contains("active") === true) ? this.classList.remove("active") : this.classList.add("active");
+    });
+  }
+
+})();
+    </script>
+    
+    </body>
+
+</html>
